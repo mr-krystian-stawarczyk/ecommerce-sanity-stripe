4e43aaae84e3e7589d69e34f36b913364ccf5156
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-02-01 10:27:22 +0100

    redirect from checkout to shop

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -68,7 +68,7 @@ export default async function handler(req, res) {
 					};
 				}),
 				success_url: `${req.headers.origin}/success`,
-				cancel_url: `${req.headers.origin}/canceled`,
+				cancel_url: `${req.headers.origin}/shop`,
 			};
 
 			// Create Checkout Sessions from body params.
